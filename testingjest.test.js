d1357e9d22a201f725d77aef2fb8006b66a9bc90
Dawid Urbaniak <dawid1745@gmail.com>
2022-01-05 00:35:29 +0100

    Added html and styled

---
+++ testingjest.test.js
@@ -0,0 +1,15 @@
+const { expect } = require("@jest/globals");
+
+
+// Przykład
+test("Co robi", () => {
+    expect(funkcja(parametry)).toBe(4);
+});
+
+test("Checking if the function is defined", () => {
+    expect(function()).toBeDefined()
+})
+
+test("Checking if type of budget is number"), () => {
+    expect(typeof zmienna ).toBe("string"))
+}
